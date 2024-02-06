b3dc43955546661f050acb996771d2a0b3148dee
radsylph <andresa@devtop.io>
2024-02-05 20:03:35 -0400

    Created Floatinbutton.jsx

--- components/Floatinbutton.jsx
+++ components/Floatinbutton.jsx
@@ -10,28 +10,38 @@ import {
   ScrollView,
   Animated,
 } from "react-native";
-
 import Icon from "react-native-vector-icons/FontAwesome";
-
 import COLORS from "../constants/colors";
 import Card from "./Card";
+import { useNavigation } from "@react-navigation/native";
 
 const Floatinbutton = () => {
+  const navigation = useNavigation(); // Obtén el objeto navigation
+
   const [icon_1] = useState(new Animated.Value(20));
   const [icon_2] = useState(new Animated.Value(20));
+  const [icon_3] = useState(new Animated.Value(20));
+  const [icon_4] = useState(new Animated.Value(20));
 
   const [pop, setPop] = useState(false);
 
+  const test = () => {
+    navigation.navigate("Create_edit");
+  };
   const popIn = () => {
     setPop(true);
     Animated.timing(icon_1, {
-      toValue: 130,
+      toValue: 250,
       duration: 500,
       useNativeDriver: false,
     }).start();
-    setPop(true);
     Animated.timing(icon_2, {
-      toValue: 100,
+      toValue: 180,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_3, {
+      toValue: 110,
       duration: 500,
       useNativeDriver: false,
     }).start();
@@ -40,12 +50,17 @@ const Floatinbutton = () => {
   const popOut = () => {
     setPop(false);
     Animated.timing(icon_1, {
-      toValue: 40,
+      toValue: -60,
       duration: 500,
       useNativeDriver: false,
     }).start();
     Animated.timing(icon_2, {
-      toValue: 40,
+      toValue: -60,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_3, {
+      toValue: -60,
       duration: 500,
       useNativeDriver: false,
     }).start();
@@ -59,9 +74,15 @@ const Floatinbutton = () => {
         </TouchableOpacity>
       </Animated.View>
 
-      <Animated.View style={[styles.circle, { bottom: icon_2, right: icon_2 }]}>
+      <Animated.View style={[styles.circle, { bottom: icon_2 }]}>
         <TouchableOpacity>
           <Icon name="user-circle" size={25} color={COLORS.white} />
+        </TouchableOpacity>
+      </Animated.View>
+
+      <Animated.View style={[styles.circle, { bottom: icon_3 }]}>
+        <TouchableOpacity onPress={() => test()}>
+          <Icon name="sticky-note-o" size={25} color={COLORS.white} />
         </TouchableOpacity>
       </Animated.View>
 
