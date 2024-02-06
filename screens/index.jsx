f9581c446b05c200d9c40f95301d5ff7a07f6e4c
radsylph <andresa@devtop.io>
2024-02-05 20:13:46 -0400

    feat: added new screens

--- screens/index.jsx
+++ screens/index.jsx
@@ -2,10 +2,6 @@ import Login from "./Login";
 import Welcome from "./Welcome";
 import Signup from "./Signup";
 import Home from "./Home";
+import Create_edit from "./Create_edit";
 
-export{
-    Login,
-    Welcome,
-    Signup,
-    Home
-}
+export { Login, Welcome, Signup, Home, Create_edit };
