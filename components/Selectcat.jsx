cb7028ef214f0ded0e3f4364b9dc29a619422077
radsylph <andresa@devtop.io>
2024-02-07 01:08:21 -0400

    feat: added functionality to work with the parent component

--- components/Selectcat.jsx
+++ components/Selectcat.jsx
@@ -15,25 +15,54 @@ import COLORS from "../constants/colors";
 import Card from "../components/Card";
 import Floatinbutton from "../components/Floatinbutton";
 import Cathorisonal from "../components/Cathorisonal";
+import { Axios, note_endpoints } from "../constants/axios";
+import { useState, useEffect } from "react";
 
 import { MultipleSelectList } from "react-native-dropdown-select-list";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Seletcat = () => {
-  const [selected, setSelected] = React.useState([]);
+const Seletcat = ({ setCategory }) => {
+  const [selected, setSelected] = useState([]);
+  const [categories, setCategories] = useState([""]);
+  const [token, setToken] = useState("");
 
-  const data = [
-    { key: "1", value: "general", disabled: true },
-    { key: "2", value: "videojuegos" },
-    { key: "3", value: "Cameras" },
-    { key: "4", value: "Computers" },
-    { key: "5", value: "Vegetables" },
-    { key: "6", value: "Diary Products" },
-    { key: "7", value: "Drinks" },
-  ];
+  const getToken = async () => {
+    await AsyncStorage.getItem("token").then((token) => {
+      console.log(token);
+      setToken(token);
+      getCategories(token);
+    });
+  };
+
+  const getCategories = async (token) => {
+    try {
+      await Axios.get(note_endpoints.getCategories, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((response) => {
+        setCategories(response.data.categories);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+    categories.forEach((category) => {
+      console.log(category);
+    });
+  }, []);
+
+  const categoryData = categories.map((category) => ({
+    key: category._id,
+    value: category.name,
+  }));
 
   return (
     <MultipleSelectList
-      placeholder="Categoties"
+      placeholder="Categories"
       boxStyles={{
         backgroundColor: COLORS.white,
         borderColor: COLORS.light_purple,
@@ -46,9 +75,9 @@ const Seletcat = () => {
       }}
       dropdownTextStyles={{ backgroundColor: COLORS.white }}
       setSelected={(val) => setSelected(val)}
-      data={data}
+      data={categoryData}
       save="value"
-      //onSelect={() => alert(selected)}
+      onSelect={() => setCategory(selected)}
       label="Categories"
       notFoundText="No data exists"
       badgeStyles={{ backgroundColor: COLORS.light_purple }}
