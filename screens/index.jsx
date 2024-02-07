466755c17fa9872b58551f55ebd6bc4634c46772
radsylph <andresa@devtop.io>
2024-02-07 13:40:41 -0400

    feat: updated the index

--- screens/index.jsx
+++ screens/index.jsx
@@ -3,5 +3,6 @@ import Welcome from "./Welcome";
 import Signup from "./Signup";
 import Home from "./Home";
 import Create_edit from "./Create_edit";
+import Favorite from "./Favorite";
 
-export { Login, Welcome, Signup, Home, Create_edit };
+export { Login, Welcome, Signup, Home, Create_edit, Favorite };
