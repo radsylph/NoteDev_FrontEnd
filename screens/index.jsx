cb4d79f786e01879c5dd04660405ffde0987502d
radsylph <andresa@devtop.io>
2024-02-08 01:15:52 -0400

    updated index

--- screens/index.jsx
+++ screens/index.jsx
@@ -5,5 +5,17 @@ import Home from "./Home";
 import Create_edit from "./Create_edit";
 import Favorite from "./Favorite";
 import EditNote from "./Edit_note";
+import User from "./User";
+import Create_cat from "./Create_cat";
 
-export { Login, Welcome, Signup, Home, Create_edit, Favorite, EditNote };
+export {
+  Login,
+  Welcome,
+  Signup,
+  Home,
+  Create_edit,
+  Favorite,
+  EditNote,
+  User,
+  Create_cat,
+};
