1c4275ce4a95974bfdd6e1cd1702a7739f826e1c
radsylph <andresa@devtop.io>
2024-02-08 01:15:13 -0400

    feat:added new colors

--- constants/colors.jsx
+++ constants/colors.jsx
@@ -1,11 +1,12 @@
-const COLORS ={
-    purple: "#303492",
-    white: "#FFFFFF",
-    light_yellow: "#FFCD7C",
-    strong_blue: "#04DAFE",
-    light_orange: "#FFA47B",
-    grayish_white: "#9F9F9F",
-    light_purple:"#7B4E91"
-}
+const COLORS = {
+  purple: "#303492",
+  white: "#FFFFFF",
+  light_yellow: "#FFCD7C",
+  strong_blue: "#04DAFE",
+  light_orange: "#FFA47B",
+  grayish_white: "#9F9F9F",
+  light_purple: "#7B4E91",
+  emergenci: "#FF0000",
+};
 
 export default COLORS;
